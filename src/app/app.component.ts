44b27f3255308f6dbb9cc3f2594ddb2793a45ae7
Juan Henao <juanm-henao@unilibre.edu.co>
2019-08-28 21:19:05 -0500

    Tabla elementos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Parcial2808';
+
+  elementos: any =[
+
+    {nombre:'Carbono', simbolo: 'C', numeroat: '6', Peso: '12.01'},
+    {nombre:'Nitrogeno', simbolo: 'N', numeroat: '7', Peso: '14.02'},
+    {nombre:'Oxígeno', simbolo: 'O', numeroat: '8', Peso: '15.99'},
+    {nombre:'Flúor', simbolo: 'F', numeroat: '9', Peso: '18.99'},
+    {nombre:'Neón', simbolo: 'Ne', numeroat: '10', Peso: '20.17'},
+    {nombre:'Sodio', simbolo: 'Na', numeroat: '11', Peso: '22.98'},
+    {nombre:'Magnecio', simbolo: 'MG', numeroat: '12', Peso: '24.30'},
+    {nombre:'Aluminio', simbolo: 'Al', numeroat: '13', Peso: '26.98'},
+    {nombre:'Fòsforo', simbolo: 'P', numeroat: '15', Peso: '30.97'},
+   ]
+
+
 }
