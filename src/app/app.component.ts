74b9b0ea6129e35c76cbe7aa02868825d3fcd460
Juan Henao <juanm-henao@unilibre.edu.co>
2019-08-28 21:43:11 -0500

    Colores al nombre

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,15 +10,15 @@ export class AppComponent {
 
   elementos: any =[
 
-    {nombre:'Carbono', simbolo: 'C', numeroat: '6', Peso: '12.01'},
-    {nombre:'Nitrogeno', simbolo: 'N', numeroat: '7', Peso: '14.02'},
-    {nombre:'Oxígeno', simbolo: 'O', numeroat: '8', Peso: '15.99'},
-    {nombre:'Flúor', simbolo: 'F', numeroat: '9', Peso: '18.99'},
-    {nombre:'Neón', simbolo: 'Ne', numeroat: '10', Peso: '20.17'},
-    {nombre:'Sodio', simbolo: 'Na', numeroat: '11', Peso: '22.98'},
-    {nombre:'Magnecio', simbolo: 'MG', numeroat: '12', Peso: '24.30'},
-    {nombre:'Aluminio', simbolo: 'Al', numeroat: '13', Peso: '26.98'},
-    {nombre:'Fòsforo', simbolo: 'P', numeroat: '15', Peso: '30.97'},
+    {nombre:'Carbono', simbolo: 'C', numeroat: '6', Peso: '12.01', metal: 2, catego: 'Alcalino',boool: true},
+    {nombre:'Nitrogeno', simbolo: 'N', numeroat: '7', Peso: '14.02', metal: 2, catego: 'Gas noble',boool: false},
+    {nombre:'Oxígeno', simbolo: 'O', numeroat: '8', Peso: '15.99', metal: 2, catego: 'Alcalino',boool: true},
+    {nombre:'Flúor', simbolo: 'F', numeroat: '9', Peso: '18.99', metal: 2, catego: 'Alcalino',boool: true},
+    {nombre:'Neón', simbolo: 'Ne', numeroat: '10', Peso: '20.17', metal: 2, catego: 'Alcalino',boool: true},
+    {nombre:'Sodio', simbolo: 'Na', numeroat: '11', Peso: '22.98', metal: 2, catego: 'Alcalino',boool: true},
+    {nombre:'Magnecio', simbolo: 'MG', numeroat: '12', Peso: '24.30', metal: 2, catego: 'Alcalino',boool: true},
+    {nombre:'Aluminio', simbolo: 'Al', numeroat: '13', Peso: '26.98', metal: 2, catego: 'Alcalino',boool: true},
+    {nombre:'Fòsforo', simbolo: 'P', numeroat: '15', Peso: '30.97', metal: 2, catego: 'Alcalino',boool: true},
    ]
 
 
